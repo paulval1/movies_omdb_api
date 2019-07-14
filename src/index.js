fa2510cc684643129487042b5d47c83919c3049b
Paul valente <paul.val1@hotmail.com>
2019-07-14 16:58:37 +0100

    basic search function in obdb

--- src/index.js
+++ src/index.js
@@ -3,20 +3,27 @@ var secretkey = config.SECRET_KEY;
 
 const results = document.querySelector('#results');
 
-const apiUrl =  `http://www.omdbapi.com/?s=harry%20potter&apikey=${secretkey}`;
-fetch(apiUrl)
-  .then(response => response.json())
-  .then((data) => {
-    data.Search.forEach((movie) => {
-      const newMovie = `<li class="list-inline-item">
-      <img src="${movie.Poster}" />
-      <p>${movie.Title}</p>
-      </li>`
-      results.insertAdjacentHTML('beforeend', newMovie);
+const searchMovies = (keyword) => {
+  const apiUrl =  `http://www.omdbapi.com/?s=${keyword}&apikey=${secretkey}`;
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((movie) => {
+        const newMovie = `<li class="list-inline-item">
+        <img src="${movie.Poster}" />
+        <p>${movie.Title}</p>
+        </li>`
+        results.insertAdjacentHTML('beforeend', newMovie);
+      });
     });
-  })
+}
 
-
+const searchForm = document.getElementById("search-movies");
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const keyword = document.querySelector("#keyword").value;
+  searchMovies(keyword);
+});
 
 
 
